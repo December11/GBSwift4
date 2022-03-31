031a1fe318108c006c67a618e296bc42ec681a61
Alla Shkolnik <5106275+December11@users.noreply.github.com>
2022-03-31 03:10:56 +0300

    ячейка новостей разбита на header, footer и ячейки для текста и фоток поотдельности

---
+++ VKApp/Extensions/UIView+Nib.swift
@@ -0,0 +1,14 @@
+//
+//  UIView+Nib.swift
+//  VKApp
+//
+//  Created by Alla Shkolnik on 31.03.2022.
+//
+
+import UIKit
+
+extension UIView {
+    class func fromNib<T: UIView>() -> T {
+        return Bundle(for: T.self).loadNibNamed(String(describing: T.self), owner: nil, options: nil)![0] as! T
+    }
+}
---
+++ VKApp/View/Reusable Cells/TableCells/FeedFooterView.swift
@@ -0,0 +1,86 @@
+//
+//  FeedFooterView.swift
+//  VKApp
+//
+//  Created by Alla Shkolnik on 31.03.2022.
+//
+
+import UIKit
+
+class FeedFooterView: UITableViewHeaderFooterView {
+    
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var replyButton: UIButton!
+    @IBOutlet weak var shareButton: UIButton!
+    @IBOutlet weak var viewsCountLabel: UILabel!
+    @IBOutlet weak var separator: UIView!
+    
+    var feed: Feed?
+    var onShare: () -> () = {}
+    
+    func configurateFooter(feed: Feed, onShare: @escaping () -> ()) {
+        self.feed = feed
+        self.onShare = onShare
+        
+        loadLikes(of: feed)
+        loadComments(of: feed)
+        loadViews(of: feed)
+    }
+    
+    override func awakeFromNib() {
+        likeButton.configuration?.background.backgroundColor = .clear
+    }
+    
+    //MARK: - Private functions
+    private func loadLikes(of feed: Feed) {
+        feed.likesCount += feed.isLiked ? 1 : 0
+        self.likeButton.setTitle(String(feed.likesCount), for: .init())
+    }
+    
+    private func loadComments(of feed: Feed) {
+        self.replyButton.setTitle(String(feed.commentsCount), for: .init())
+    }
+    
+    private func loadViews(of feed: Feed) {
+        self.viewsCountLabel.text = String(feed.viewsCount)
+    }
+    
+    //MARK: - Animations
+    private func likeAnimate() {
+        UIView.transition(with: self.likeButton, duration: 0.1, options: .transitionCrossDissolve) { [self] in
+            let image = likeButton.isSelected
+            ? UIImage(systemName: "hand.thumbsup.circle.fill")
+            : UIImage(systemName: "hand.thumbsup.circle")
+            likeButton.setImage(image, for: .init())
+        }
+        
+        UIView.animate(withDuration: 0.3, delay: 0.1, usingSpringWithDamping: 0.4, initialSpringVelocity: 0.5, options: .curveEaseInOut) { [self] in
+            likeButton.imageView?.frame.origin.y += 1
+        } completion: { [self] isCompletion in
+            likeButton.imageView?.frame.origin.y -= 1
+        }
+    }
+    
+//    private func tapAtImageAnimate() {
+//        UIView.animate(withDuration: 2.0, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseInOut) { [self] in
+//            userPhoto.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+//        } completion: { isCompletion in
+//            print("tapped on image")
+//        }
+//    }
+    
+    //MARK: - IBActions
+    @IBAction func like(_ sender: UIButton) {
+        sender.isSelected.toggle()
+        feed?.isLiked.toggle()
+        
+        let count = feed?.likesCount ?? 0
+        sender.setTitle(String(count), for: .init())
+        likeAnimate()
+    }
+    
+    @IBAction func share(_ sender: Any) {
+        self.onShare()
+    }
+
+}
